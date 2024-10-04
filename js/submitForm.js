48ed33c639dc10fa53860a29902b2b35f7e5014e
Giang-LHG <giangx16@gmail.com>
2024-10-04 14:07:46 +0700

    update app Vesion_2

--- js/submitForm.js
+++ js/submitForm.js
@@ -3,6 +3,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   let data = new FormData(form);
   fetch(
-    "https://script.google.com/macros/s/AKfycbzqxuQBw44BvnaafJtTg02sRDKHoEzdOHyqKaHzyu3NjF98WC00UoNE1YahizKgyzj3Lw/exec"
+    "https://script.google.com/macros/s/AKfycbzqxuQBw44BvnaafJtTg02sRDKHoEzdOHyqKaHzyu3NjF98WC00UoNE1YahizKgyzj3Lw/exec",
+    { method: "POST", body: data }
   ).then((res) => res.text().then((data) => console.log(data)));
 });
